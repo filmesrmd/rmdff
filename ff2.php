3fed4feb22f0a4cc3417504b17e294abe159cc52
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-08-22 01:02:26 -0300

    Add files via upload

--- ff2.php
+++ ff2.php
@@ -1,20 +1,19 @@
+<body>
 <?php
+$v = exec("ping " . "version.common.freefiremobile.com");
+$h = explode('ms', $v);
+$i =  $h[2];
+$media = explode("=",$i);
 
-$link = json_decode(file_get_contents('https://rmdff.vercel.app/api/apiff.phpW'));
-$nome_ff = $link->ff->name;
-$nome_da_versao_ff = $link->ff->version_name;
-$codigo_da_versao_ff = $link->ff->version_code;
-$status_ff = $link->ff->status;
-$versao_ff = $link->ff->version_atual;
-$proxima_versao_ff = $link->ff->version_disponivel;
-$atualizacao = $link->ff->next_version_date;
+echo "<h2>TESTE Ping FF</h2>";
+if($media[1]>999){$media = '<h4 id = "ping_style">Ping: 999+</h4>';echo $media;}else{echo '<h4 id = "ping_style">Ping: '.$media[1].'</h4>';}
 ?>
+    <script>
+    var ping =<?php echo$media[1];?>
 
-<h4><?php echo "Nome: " . $nome_ff; ?></h4>
-<h4><?php echo "Versão: " . $nome_da_versao_ff . " (" . $codigo_da_versao_ff . ")" ; ?></h4>
-<h4><?php echo "Status: " . $status_ff; ?></h4>
-<h4><?php echo "Versão Atual: " . $versao_ff; ?></h4>
-<h4><?php echo "Próxima Versão Disponível: " . $proxima_versao_ff; ?></h4>
-<h4><?php echo "Próxima Atualizacão: " . $atualizacao; ?></h4>
-
-
+        if(ping<=120&&ping>0){ping_style.style.color="green"}
+        if(ping<=300&&ping>=120){ping_style.style.color="#D7DF01"}
+        if(ping<=600&&ping>=320){ping_style.style.color="orange"}
+        if(ping>=600){ping_style.style.color="red"}
+    </script>
+</body>

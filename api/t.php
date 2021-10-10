36a8bc3a1c7aeae655f662c0ff148bf3430f2160
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-10-10 16:30:02 -0300

    Add files via upload

---
+++ api/t.php
@@ -0,0 +1,11 @@
+enviado pelo HTML<br>
+  <script>
+   document.write("enviado por JS")
+  </script>
+
+<?php
+echo file_get_contents("https://ffshare.garena.com");
+?>
+<style>
+  iframe {width:100%;height:100%;}
+</style>
